71f1cf9b6294fb709a83e9c15666f1b7a73e9e8d
Saad-1719 <chsaad1407@gmail.com>
2025-03-12 17:09:36 +0500

    Add Customer Review Module and enhance image accessibility

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
 import AboutSection from "../components/AboutSection";
+import CustomerReviews from "../components/CustomerReviews";
 const MainPage = () => {
 	return (
-		<section className='h-[calc(100lvh-56px)] bg-[url("/hero-img.png")] bg-center object-cover'>
+		<section className=''>
 			{/* hero section */}
-			<div className="max-w-2xl mx-auto flex flex-col items-center justify-center h-full text-center space-y-4">
+			<div className=" bg-[url('/hero-img.png')] bg-center object-cover h-[calc(100lvh-56px)]  mx-auto flex flex-col items-center justify-center text-center space-y-4">
 				<h1 className="text-8xl headline">Best food for your taste</h1>
 				<p className="text-lg max-w-lg mx-auto">
 					Discover delectable cuisine and unforgettable moments in our
@@ -340,52 +341,7 @@ const MainPage = () => {
 				</Wrapper>
 			</div>
 			{/* Customer Reviews */}
-			<div className="py-24">
-				<Wrapper>
-				<h1 className="pb-10 text-6xl text-center headline">
-						What Our Customer Says
-					</h1>
-					<div className="grid grid-cols-3 gap-10">
-						<article className="p-10 bg-[#F9F9F7] rounded-2xl">
-							<h2 className="text-2xl font-bold text-[var(--button-color)]">“The best restaurant”</h2>
-							<p className="text-lg py-4">Last night, we dined at place and were simply blown away. From the moment we stepped in, we were enveloped in an inviting atmosphere and greeted with warm smiles.</p>
-							<hr className="py-4 border-[var(--primary-color)]"/>
-							<article className="flex  items-center space-x-4 py-2"><img src="src/assets/sophie.png" alt="" />
-								<span>
-
-								<h2 className="font-bold text-black">Sophire Robson</h2>
-								<h3 className="text-black">Los Angeles, CA</h3>
-								</span>
-							</article>
-						</article>
-						<article className="p-10 bg-[#F9F9F7] rounded-2xl">
-							<h2 className="text-2xl font-bold text-[var(--button-color)]">“Simply delicious”</h2>
-							<p className="text-lg py-4">Place exceeded my expectations on all fronts. The ambiance was cozy and relaxed, making it a perfect venue for our anniversary dinner. Each dish was prepared and beautifully presented.</p>
-							<hr className="py-4"/>
-							<article className="flex  items-center space-x-4 py-2"><img src="src/assets/matt.png" alt="" />
-								<span>
-
-								<h2 className="font-bold text-black">Matt Cannon</h2>
-								<h3 className="text-black">San Diego, CA</h3>
-								</span>
-							</article>
-						</article>
-						<article className="p-10 bg-[#F9F9F7] rounded-2xl">
-							<h2 className="text-2xl font-bold text-[var(--button-color)]">“One of a kind restaurant”</h2>
-							<p className="text-lg py-4">The culinary experience at place is first to none. The atmosphere is vibrant, the food - nothing short of extraordinary. The food was the highlight of our evening. Highly recommended.</p>
-							<hr className="py-4"/>
-							<article className="flex  items-center space-x-4 py-2"><img src="src/assets/andy.png" alt="" />
-								<span>
-
-								<h2 className="font-bold text-black">Andy Smith</h2>
-								<h3 className="text-black">San Francisco, CA</h3>
-								</span>
-							</article>
-						</article>
-					</div>
-				</Wrapper>
-
-			</div>
+			<CustomerReviews />
 		</section>
 	);
 };
