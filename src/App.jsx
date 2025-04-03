4da379f836d5bba5d8dc35b55b16b6e00edda39a
Saad-1719 <chsaad1407@gmail.com>
2025-04-04 03:43:33 +0500

    Add framer-motion dependency and implement CartSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import ContactUs from "./pages/ContactUs";
 import MenuPage from "./pages/MenuPage";
 import CartPage from "./pages/CartPage";
 import ProtectedCheckout from "./pages/ProtectedCheckout";
 import NotFound from "./components/NotFound";
+import CartSidebar from "./components/CartSidebar";
 
 const App = () => {
+	const [isCartOpen, setIsCartOpen] = useState(false);
+
 	return (
 		<>
-			<Header />
+			<Header onCartClick={() => setIsCartOpen(true)} />
 			<Routes>
 				<Route path="/" element={<MainPage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/menu" element={<MenuPage />} />
 				<Route path="/contact" element={<ContactUs />} />
-				<Route path="/cart" element={<CartPage />} />
+				{/* <Route path="/cart" element={<CartPage />} /> */}
 				<Route path="/checkout" element={<ProtectedCheckout />} />
-				<Route path="*" element={<NotFound/>} />
-
+				<Route path="*" element={<NotFound />} />
 			</Routes>
+			<CartSidebar isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
 			<Footer />
-			
 		</>
 	);
 };
