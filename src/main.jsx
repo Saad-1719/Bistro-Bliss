5affaa6e2bcb591a6c6af21472fd58484bc3a6c7
Saad-1719 <chsaad1407@gmail.com>
2025-04-04 03:43:42 +0500

    Remove unused Header and Footer imports from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
 import { BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
